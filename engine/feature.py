fe8dfe9a2fedf9c379c8bb808da14aac3193e4d7
RyderHuangSABR <ryder99.huang@gmail.com>
2026-03-29 14:11:43 +0800

    Create feature.py

---
+++ engine/feature.py
@@ -0,0 +1,12 @@
+FEATURES = [
+    'pitch_type', 'release_speed', 'effective_speed', 'release_spin_rate', 'spin_axis',
+    'pfx_x', 'pfx_z', 'release_pos_x', 'release_pos_z', 'release_extension', 
+    'plate_x', 'plate_z', 'vaa', 'haa', 'commit_x', 'commit_z', 'reaction_time', 'movement_ratio'
+]
+
+PITCH_GROUPS = {
+    "FF": "Fastball", "SI": "Sinker", "FC": "Cutter", "CH": "Changeup",
+    "FS": "SplitterFork", "FO": "SplitterFork", "ST": "SweeperSlider",
+    "SL": "SweeperSlider", "SW": "SweeperSlider", "CU": "Curveball",
+    "KC": "Curveball", "CS": "Curveball"
+}
